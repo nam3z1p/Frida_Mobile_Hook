98397261dfdcfc6c84880faa82a734c566b28dca
nam3z1p <38063224+nam3z1p@users.noreply.github.com>
2021-05-10 20:03:12 +0900

    Add files via upload

---
+++ iOS_ClassDump.js
@@ -0,0 +1,27 @@
+console.log("[+] Script loaded successfully ");
+
+
+if (ObjC.available){
+	var matchstring = 'check';
+    for (var className in ObjC.classes){
+		if (ObjC.classes.hasOwnProperty(className)){
+			//send("[*] "+className);
+			if(className.toLowerCase().indexOf(matchstring.toLowerCase()) >= 0){
+                send("\n\n")
+                send("========================================================================");
+                send("[*] "+className);
+                send("========================================================================");
+	
+				var methods = ObjC.classes[className].$ownMethods;
+				
+				for (var i = 0; i < methods.length; i++){
+					send(methods[i]);
+				}
+			}
+		}
+    }
+}
+else{
+    console.log("Objective-C Runtime is not available!");
+}	
+
